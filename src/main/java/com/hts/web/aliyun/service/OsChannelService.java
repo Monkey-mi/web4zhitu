5d0998d64ad2049d47a731f1fb4ea9b58958ad4a
lynch <zhutianjie89@gmail.com>
2015-05-13 09:40:34 +0800

    添加labelIds参数

--- src/main/java/com/hts/web/aliyun/service/OsChannelService.java
+++ src/main/java/com/hts/web/aliyun/service/OsChannelService.java
@@ -1,14 +1,59 @@
 package com.hts.web.aliyun.service;
 
+/**
+ * OpenSearch channel业务逻辑访问接口
+ * 
+ * @author lynch
+ *
+ */
 public interface OsChannelService {
 
-	public void saveChannelAtOnce(Integer id, Integer ownerId, String channelName, 
-			String channelTitle, String subtitle, String channelDesc, String channelIcon, String subIcon,
-			Integer channelType, String channelLabel, Integer danmu, Integer mood, Integer world);
-	
-	
-	public void updateChannelAtOnce(Integer id, Integer ownerId, String channelName, 
-			String channelTitle, String subtitle, String channelDesc, String channelIcon, String subIcon,
-			Integer channelType, String channelLabel, Integer danmu, Integer mood, Integer world);
-	
+	/**
+	 * 立即保存频道索引
+	 * 
+	 * @param id
+	 * @param ownerId
+	 * @param channelName
+	 * @param channelTitle
+	 * @param subtitle
+	 * @param channelDesc
+	 * @param channelIcon
+	 * @param subIcon
+	 * @param channelType
+	 * @param channelLabel
+	 * @param labelIds
+	 * @param danmu
+	 * @param mood
+	 * @param world
+	 */
+	public void saveChannelAtOnce(Integer id, Integer ownerId,
+			String channelName, String channelTitle, String subtitle,
+			String channelDesc, String channelIcon, String subIcon,
+			Integer channelType, String channelLabel, String labelIds,
+			Integer danmu, Integer mood, Integer world);
+
+	/**
+	 * 立即更新频道索引
+	 * 
+	 * @param id
+	 * @param ownerId
+	 * @param channelName
+	 * @param channelTitle
+	 * @param subtitle
+	 * @param channelDesc
+	 * @param channelIcon
+	 * @param subIcon
+	 * @param channelType
+	 * @param channelLabel
+	 * @param labelIds
+	 * @param danmu
+	 * @param mood
+	 * @param world
+	 */
+	public void updateChannelAtOnce(Integer id, Integer ownerId,
+			String channelName, String channelTitle, String subtitle,
+			String channelDesc, String channelIcon, String subIcon,
+			Integer channelType, String channelLabel, String labelIds,
+			Integer danmu, Integer mood, Integer world);
+
 }
--- src/main/java/com/hts/web/aliyun/service/impl/OsChannelServiceImpl.java
+++ src/main/java/com/hts/web/aliyun/service/impl/OsChannelServiceImpl.java
@@ -11,20 +11,21 @@ public class OsChannelServiceImpl implements OsChannelService {
 	public void saveChannelAtOnce(Integer id, Integer ownerId,
 			String channelName, String channelTitle, String subtitle,
 			String channelDesc, String channelIcon, String subIcon,
-			Integer channelType, String channelLabel, Integer danmu,
-			Integer mood, Integer world) {
+			Integer channelType, String channelLabel, String labelIds,
+			Integer danmu, Integer mood, Integer world) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void updateChannelAtOnce(Integer id, Integer ownerId,
 			String channelName, String channelTitle, String subtitle,
 			String channelDesc, String channelIcon, String subIcon,
-			Integer channelType, String channelLabel, Integer danmu,
-			Integer mood, Integer world) {
+			Integer channelType, String channelLabel, String labelIds,
+			Integer danmu, Integer mood, Integer world) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
 
 }
