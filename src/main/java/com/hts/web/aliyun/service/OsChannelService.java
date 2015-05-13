66565f6fc945ce8c9eb72901af2ffa49fbaedc1c
lynch <zhutianjie89@gmail.com>
2015-05-13 11:03:31 +0800

    批量更新频道索引

--- src/main/java/com/hts/web/aliyun/service/OsChannelService.java
+++ src/main/java/com/hts/web/aliyun/service/OsChannelService.java
@@ -1,5 +1,9 @@
 package com.hts.web.aliyun.service;
 
+import java.util.List;
+
+import com.hts.web.common.pojo.OpChannel;
+
 /**
  * OpenSearch channel业务逻辑访问接口
  * 
@@ -30,10 +34,18 @@ public interface OsChannelService {
 			String channelName, String channelTitle, String subtitle,
 			String channelDesc, String channelIcon, String subIcon,
 			Integer channelType, String channelLabel, String labelIds,
-			Integer danmu, Integer mood, Integer world);
+			Integer danmu, Integer mood, Integer world) throws Exception;
 
 	/**
-	 * 立即更新频道索引
+	 * 批量保存频道
+	 * 
+	 * @param channels
+	 * @throws Exception
+	 */
+	public void saveChannelAtOnce(List<OpChannel> channels) throws Exception;
+	
+	/**
+	 *　更新频道
 	 * 
 	 * @param id
 	 * @param ownerId
@@ -54,6 +66,31 @@ public interface OsChannelService {
 			String channelName, String channelTitle, String subtitle,
 			String channelDesc, String channelIcon, String subIcon,
 			Integer channelType, String channelLabel, String labelIds,
-			Integer danmu, Integer mood, Integer world);
+			Integer danmu, Integer mood, Integer world) throws Exception;
+	
+	/**
+	 * 批量更新频道
+	 * 
+	 * @param channels
+	 * @throws Exception
+	 */
+	public void updateChannelAtOnce(List<OpChannel> channels) throws Exception;
+	
+	
+	/**
+	 * 立即删除频道
+	 * 
+	 * @param id
+	 * @throws Exception
+	 */
+	public void deleteChannelAtOnce(Integer id) throws Exception;
+	
+	/**
+	 * 根据ids立即删除频道
+	 * 
+	 * @param ids
+	 * @throws Exception
+	 */
+	public void deleteChannelAtOnce(Integer[] ids) throws Exception;
 
 }
--- src/main/java/com/hts/web/aliyun/service/impl/OsChannelServiceImpl.java
+++ src/main/java/com/hts/web/aliyun/service/impl/OsChannelServiceImpl.java
@@ -1,20 +1,36 @@
 package com.hts.web.aliyun.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hts.web.aliyun.dao.OsChannelDao;
 import com.hts.web.aliyun.service.OsChannelService;
+import com.hts.web.common.pojo.OpChannel;
 
 @Service("HTSOsChannelService")
 public class OsChannelServiceImpl implements OsChannelService {
 
+	@Autowired
+	private OsChannelDao channelDao;
+	
 	@Override
 	public void saveChannelAtOnce(Integer id, Integer ownerId,
 			String channelName, String channelTitle, String subtitle,
 			String channelDesc, String channelIcon, String subIcon,
 			Integer channelType, String channelLabel, String labelIds,
-			Integer danmu, Integer mood, Integer world) {
-		// TODO Auto-generated method stub
-		
+			Integer danmu, Integer mood, Integer world) throws Exception {
+		OpChannel chan = new OpChannel();
+		chan.setId(id);
+		chan.setChannelName(channelName);
+		chan.setSubtitle(subtitle);
+		chan.setChannelDesc(channelDesc);
+		chan.setChannelIcon(channelIcon);
+		chan.setChannelType(channelType);
+		chan.setChannelLabel(channelLabel);
+		chan.setLabelIds(labelIds);
+		channelDao.saveChannel(chan);
 	}
 
 	@Override
@@ -22,8 +38,35 @@ public class OsChannelServiceImpl implements OsChannelService {
 			String channelName, String channelTitle, String subtitle,
 			String channelDesc, String channelIcon, String subIcon,
 			Integer channelType, String channelLabel, String labelIds,
-			Integer danmu, Integer mood, Integer world) {
-		// TODO Auto-generated method stub
+			Integer danmu, Integer mood, Integer world) throws Exception{
+		OpChannel chan = new OpChannel();
+		chan.setId(id);
+		chan.setChannelName(channelName);
+		chan.setSubtitle(subtitle);
+		chan.setChannelDesc(channelDesc);
+		chan.setChannelIcon(channelIcon);
+		chan.setChannelType(channelType);
+		chan.setChannelLabel(channelLabel);
+		chan.setLabelIds(labelIds);
+		channelDao.updateChannel(chan);
+	}
+
+	@Override
+	public void deleteChannelAtOnce(Integer id) throws Exception {
+		channelDao.deleteChannelById(id);
+	}
+	
+	public void deleteChannelAtOnce(Integer[] ids) throws Exception {
+		
+	}
+
+	@Override
+	public void saveChannelAtOnce(List<OpChannel> channels) throws Exception {
+		
+	}
+
+	@Override
+	public void updateChannelAtOnce(List<OpChannel> channels) throws Exception {
 		
 	}
 
