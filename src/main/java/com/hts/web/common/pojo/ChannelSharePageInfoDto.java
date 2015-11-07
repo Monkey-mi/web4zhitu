f1d6ebb5dd6f1d9ee179601811765dfbf37a8de3
mishengliang <mishengliang@mike-Inspiron-5548>
2015-11-07 15:47:30 +0800

    频道分享页面

---
+++ src/main/java/com/hts/web/common/pojo/ChannelSharePageInfoDto.java
@@ -0,0 +1,42 @@
+package com.hts.web.common.pojo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.struts2.json.annotations.JSON;
+
+/**
+ * 频道分享页面信息
+ * 频道信息
+ * 精选织图信息
+ * 11-07-2015
+ * @author mishengliang
+ *
+ */
+public class ChannelSharePageInfoDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1703834474706727159L;
+
+
+	private OpChannelDetail opChannelDetail;
+	private List<OpChannelWorldDto> opChannelWorldDtos;
+	
+	public OpChannelDetail getOpChannelDetail() {
+		return opChannelDetail;
+	}
+	public void setOpChannelDetail(OpChannelDetail opChannelDetail) {
+		this.opChannelDetail = opChannelDetail;
+	}
+	public List<OpChannelWorldDto> getOpChannelWorldDto() {
+		return opChannelWorldDtos;
+	}
+	public void setOpChannelWorldDto(List<OpChannelWorldDto> opChannelWorldDtos) {
+		this.opChannelWorldDtos = opChannelWorldDtos;
+	}
+	
+}
