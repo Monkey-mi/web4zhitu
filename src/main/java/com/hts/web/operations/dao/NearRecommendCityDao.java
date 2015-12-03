61f4e747000cbfc288d1be8fe7d120af370077a1
zengxianxian <474183700@qq.com>
2015-12-03 17:39:22 +0800

    新增附近推荐城市

---
+++ src/main/java/com/hts/web/operations/dao/NearRecommendCityDao.java
@@ -0,0 +1,27 @@
+package com.hts.web.operations.dao;
+
+import java.util.List;
+
+import com.hts.web.common.pojo.AddrCity;
+import com.hts.web.common.pojo.OpNearCityGroupDto;
+
+/**
+ * 附近推荐城市持久层接口
+ * @author zxx 2015-12-3 16:46:26
+ *
+ */
+public interface NearRecommendCityDao {
+	/**
+	 * 根据城市组id，查询城市组
+	 * @param cityGroupId
+	 * @return
+	 * @author zxx 2015-12-3 16:49:51
+	 */
+	public List<AddrCity> queryNearRecommendCityByGroupId(Integer cityGroupId);
+	
+	/**
+	 * 查询所有的城市推荐分组
+	 * @return
+	 */
+	public List<OpNearCityGroupDto> queryNearCityGroup();
+}
