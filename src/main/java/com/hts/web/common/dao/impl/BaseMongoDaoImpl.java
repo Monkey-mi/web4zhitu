d6edcbf0a80a7eeb6114b9ca80de7ed362643045
lynch <zhutianjie89@gmail.com>
2015-12-04 11:18:41 +0800

    添加附近织图DTO

--- src/main/java/com/hts/web/common/dao/impl/BaseMongoDaoImpl.java
+++ src/main/java/com/hts/web/common/dao/impl/BaseMongoDaoImpl.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import com.hts.web.common.dao.BaseMongoDao;
 
 public class BaseMongoDaoImpl implements BaseMongoDao {
-
 	
 	@Autowired
 	private MongoTemplate mongoTemplate;
