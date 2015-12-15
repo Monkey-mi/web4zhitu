fba979919aa0812084c6d24cd0a56464640ce805
lynch <zhutianjie89@gmail.com>
2015-12-15 20:19:55 +0800

    冲突解决

--- src/main/java/com/hts/web/base/database/MongoHTS.java
+++ src/main/java/com/hts/web/base/database/MongoHTS.java
@@ -7,6 +7,8 @@ public class MongoHTS {
 	 */
 	public static final String NEAR_WORLD = "near_world";
 	
+	public static final String NEAR_WORLD_LAST = "near_world_last";
+	
 	/**
 	 * 明星用户织图集合
 	 */
@@ -18,7 +20,13 @@ public class MongoHTS {
 	public static final String NEAR_LABEL = "near_label";
 	
 	/**
+	 * 附近公告
+	 */
+	public static final String NEAR_BULLETIN = "near_bulletin";
+	
+	/**
 	 * 城市信息
 	 */
 	public static final String ADDR_CITY = "addr_city";
+	
 }
